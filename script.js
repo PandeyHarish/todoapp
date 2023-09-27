faf86101c2061d26b5ed86f5c3274b5f59bc8403
Harish Pandey <pandeyharish394@gmail.com>
2023-09-27 21:26:18 +0545

    changed the to jquery

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-var inputValue = document.getElementById("inputBox");
-var list = document.getElementById("list");
-var btn = document.getElementById("btn");
+// var inputValue = document.getElementById("inputBox");
+// var list = document.getElementById("list");
+// var btn = document.getElementById("btn");
 
-btn.addEventListener("click", function () {
-  if (inputValue.value.trim() === "") {
-    alert("List cannot be empty");
-  } else {
-    var newElement = document.createElement("li");
-    var text = document.createTextNode(inputValue.value); 
-    newElement.appendChild(text);
-    list.appendChild(newElement);
-  }
+// btn.addEventListener("click", function (e) {
+
+//   if (inputValue.value.trim() === "") {
+//     alert("List cannot be empty");
+//   } else {
+//     var newElement = document.createElement("li");
+//     var text = document.createTextNode(inputValue.value);
+//     newElement.appendChild(text);
+//     list.appendChild(newElement);
+//   }
+// });
+
+$(document).ready(function () {
+  var list = $("#list");
+  var btn = $("#btn");
+
+  $("#btn").click(function (e) {
+    let inputValue = $("#inputBox").val().trim();
+
+    if (inputValue === "") {
+      alert("List cannot be empty");
+    } else {
+      let newElement = list.append("<li>");
+      let text = inputValue;
+      newElement.append(text);
+    }
+  });
 });
